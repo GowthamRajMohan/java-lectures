2002d7241d79acecdcf7005e02250f7ffccba4cc
gowtham <mohangowthamraj@gmail.com>
2022-05-21 18:49:39 +0530

    first-commit

---
+++ src/io/smacs/Besant.java
@@ -0,0 +1,7 @@
+package io.smacs;
+
+public class Besant {
+
+
+
+}
---
+++ src/io/smacs/Main.java
@@ -0,0 +1,17 @@
+package io.smacs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 100;
+        String name = "java";
+        boolean isDone = true;
+
+        System.out.println("Hello world");
+        System.out.println(n);
+        System.out.println(name);
+        System.out.println(isDone);
+    }
+
+
+}
